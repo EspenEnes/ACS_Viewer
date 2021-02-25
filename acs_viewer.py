4cb9934404a060845bdfa31568c7fe1cce67e865
enese <espen.enes@nov.com>
2021-02-25 13:29:28 +0100

    Removed transfer(pyqtsignal) Run and Snapshot buttons from mainwindow view. new logic for contextmenu on listview added. Added toolbar with Run button Added statusbar and detaild docked widget Added detail button in statusbar to open detailed view widget. Refactor MyOPENGL class to seperate import myopengl
    Snap7 exeptions to be written in detailed view

--- acs_viewer.py
+++ acs_viewer.py
@@ -1,14 +1,10 @@
 import sys
-from math import cos, sin, radians
 
-import numpy
 import snap7
-from OpenGL.GL import *
-from OpenGL.GLU import gluPerspective, gluLookAt
-from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtCore import QRunnable, QThreadPool, QObject, pyqtSignal, QEvent, Qt
-from PyQt5.QtGui import QMouseEvent, QWheelEvent
-from PyQt5.QtWidgets import QApplication, QMenu, QAction
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtCore import QRunnable, QThreadPool, QObject, pyqtSignal, QEvent
+from PyQt5.QtWidgets import QApplication
+from snap7.snap7exceptions import Snap7Exception
 
 import machine_config
 from QT_Design import main_ACS
@@ -17,7 +13,7 @@ from QT_Design import main_ACS
 class WorkerSignals(QObject):
     Start = pyqtSignal(bool)
     Data = pyqtSignal(dict)
-
+    error = pyqtSignal(str)
 
 class PlcWorker(QRunnable):
     def __init__(self, client, data):
@@ -29,11 +25,16 @@ class PlcWorker(QRunnable):
         self.Start = True
 
     def run(self):
-        self.client.connect(self.data["PLC"][0], int(self.data["PLC"][1]), int(self.data["PLC"][2]))
+        try:
+            self.client.connect(self.data["PLC"][0], int(self.data["PLC"][1]), int(self.data["PLC"][2]))
+        except Snap7Exception as e:
+            self.signals.error.emit(str(e))
+
+
         send = {}
         send["machines"] = {}
 
-        while self.Start:
+        while self.Start and self.client.get_connected():
             DB = self.client.db_get(int(self.data["PLC"][3]))
 
             for machine in self.data["Machine"].keys():
@@ -69,14 +70,11 @@ class ACSviewer(QtWidgets.QMainWindow, main_ACS.Ui_MainWindow):
         super(ACSviewer, self).__init__(parent)
         self.setupUi(self)
         self.worker = None
-        self.openGLWidget = MyOPENGL(self)
         self.openGLWidget.setObjectName("openGLWidget")
         self.gridLayout.addWidget(self.openGLWidget, 0, 0, 1, 2)
 
         self.machConf_Dialog = machine_config.MachineConfig(self)
         self.actionACS.triggered.connect(self.test)
-        self.pushButton.setCheckable(True)
-        self.pushButton.clicked.connect(self.startcollector)
         self.clientConnected = False
         self.threadpool = QThreadPool()
 
@@ -85,19 +83,28 @@ class ACSviewer(QtWidgets.QMainWindow, main_ACS.Ui_MainWindow):
         self.slot = self.machConf_Dialog.model._data["PLC"][2]
         self.DB = self.machConf_Dialog.model._data["PLC"][3]
         self.client = snap7.client.Client()
-        self.pushButton.setStyleSheet(f"background-color: green")
         self.listView_2.setModel(self.machConf_Dialog.model)
+        self.run = QtWidgets.QPushButton("Run")
+        self.run.setIcon(QtGui.QIcon("QT_Design/play-16.png"))
+        self.run.setCheckable(True)
+        self.run.clicked.connect(self.startcollector)
 
-        self.listView_2.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.listView_2.customContextMenuRequested[QtCore.QPoint].connect(self.rightMenuShow)
+        self.details = QtWidgets.QPushButton("Details")
+        self.details.clicked.connect(lambda : self.dockWidget.show())
+        spacer = QtWidgets.QWidget()
+        spacer.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.toolBar.addWidget(spacer)
+        self.toolBar.addWidget(self.run)
+        self.statusbar.addWidget(self.details)
 
-    def rightMenuShow(self):
-        rightMenu = QMenu(self.listView_2)
-        hideAction = QAction(u"Hide", self, triggered=lambda: self.hidei("Hide"))
-        rightMenu.addAction(hideAction)
-        hideAction = QAction(u"UnHide", self, triggered=lambda: self.hidei("UnHide"))
-        rightMenu.addAction(hideAction)
-        rightMenu.exec_(QtGui.QCursor.pos())
+        #Add Actions to right click menu bar
+        self.actionHide.triggered.connect(lambda : self.hidei("Hide"))
+        self.actionUnhide.triggered.connect(lambda : self.hidei("UnHide"))
+        self.actionDetails.triggered.connect(self.Machdetails)
+
+        self.listView_2.addAction(self.actionHide)
+        self.listView_2.addAction(self.actionUnhide)
+        self.listView_2.addAction(self.actionDetails)
 
     def hidei(self, cmd):
         indexes = self.listView_2.selectedIndexes()
@@ -108,6 +115,12 @@ class ACSviewer(QtWidgets.QMainWindow, main_ACS.Ui_MainWindow):
                 self.machConf_Dialog.model._data["Machine"][keys[index.row()]]["Visible"] = "False"
             elif cmd == "UnHide":
                 self.machConf_Dialog.model._data["Machine"][keys[index.row()]]["Visible"] = "True"
+
+    def Machdetails(self):
+        indexes = self.listView_2.selectedIndexes()
+        if len(indexes) > 1:
+            self.setDetailedText("Select only one machine to view details from")
+            return False
 
     def eventFilter(self, watched, event):
         if self.machConf_Dialog.isActiveWindow():
@@ -127,6 +140,7 @@ class ACSviewer(QtWidgets.QMainWindow, main_ACS.Ui_MainWindow):
         if cheched:
             self.worker = PlcWorker(self.client, self.machConf_Dialog.model._data)
             self.worker.signals.Data.connect(self.collector)
+            self.worker.signals.error.connect(self.setDetailedText)
             self.clientConnected = True
             self.threadpool.start(self.worker)
 
@@ -138,245 +152,8 @@ class ACSviewer(QtWidgets.QMainWindow, main_ACS.Ui_MainWindow):
         self.openGLWidget.Data = data
         self.worker._data = self.machConf_Dialog.model._data
 
-
-class MyOPENGL(QtWidgets.QOpenGLWidget):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-        self.up_down_angle = 0.0
-        self.diff = QtCore.QPoint(0, 0)
-        self.left_right_angle = 0.0
-
-        timer = QtCore.QTimer(self)
-        timer.setInterval(20)  # period, in milliseconds
-        timer.timeout.connect(self.update)
-        timer.start()
-
-        self.cameraPos = [0.0, 0.0, -10]
-        self.cameraFront = [0.0, 0.0, -9]
-        self.cameraUp = [0.0, 1.0, 0.0]
-        self.left_right_angle = -90.0
-
-    def initializeGL(self):
-        glClearColor(0.85, 0.85, 0.85, 1.0)
-        glEnable(GL_DEPTH_TEST)
-
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        glEnable(GL_BLEND)
-        self.Data = {}
-        self.Data["machines"] = {}
-        self.Data["origo"] = [[10, False], [10, False], [0, False]]
-        self.fwd = False
-        self.up = False
-        self.bwd = False
-        self.down = False
-        self.A = False
-        self.left = False
-        self.D = False
-        self.right = False
-
-    def resizeGL(self, width, height):
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        glViewport(1, 1, width, height)
-        gluPerspective(45.0, width / height, 0.1, 200.0)
-
-    def CamereMove(self):
-
-        if self.fwd:
-            self.cameraPos[0] -= (self.cameraFront[0] * 0.5)
-            self.cameraPos[1] -= (self.cameraFront[1] * 0.5)
-            self.cameraPos[2] -= (self.cameraFront[2] * 0.5)
-        if self.bwd:
-            self.cameraPos[0] += (self.cameraFront[0] * 0.5)
-            self.cameraPos[1] += (self.cameraFront[1] * 0.5)
-            self.cameraPos[2] += (self.cameraFront[2] * 0.5)
-        if self.up: self.diff = QtCore.QPoint(0, -100)
-        if self.down: self.diff = QtCore.QPoint(0, +100)
-        if self.left: self.diff = QtCore.QPoint(-100, 0)
-        if self.A:
-            a = numpy.cross(self.cameraFront, self.cameraUp)
-            self.cameraPos[0] += (a[0] * 0.5)
-            self.cameraPos[1] += (a[1] * 0.5)
-            self.cameraPos[2] += (a[2] * 0.5)
-        if self.D:
-            a = numpy.cross(self.cameraFront, self.cameraUp)
-            self.cameraPos[0] -= (a[0] * 0.5)
-            self.cameraPos[1] -= (a[1] * 0.5)
-            self.cameraPos[2] -= (a[2] * 0.5)
-        if self.right:   self.diff = QtCore.QPoint(100, 0)
-
-        self.up_down_angle += self.diff.y() * 0.01
-        self.left_right_angle += self.diff.x() * 0.01
-
-        direction = [0, 0, 0]
-        direction[0] = cos(radians(self.left_right_angle)) * cos(radians(self.up_down_angle))
-        direction[1] = sin(radians(self.up_down_angle))
-        direction[2] = sin(radians(self.left_right_angle)) * cos(radians(self.up_down_angle))
-
-        self.cameraFront = (direction[0], direction[1], direction[2])
-        self.diff = QtCore.QPoint(0, 0)
-
-        return self.cameraPos + (list(map(lambda i, j: i - j, self.cameraPos, self.cameraFront)) + self.cameraUp)
-
-    def paintGL(self):
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glMatrixMode(GL_MODELVIEW)
-
-        glLoadIdentity()
-        gluLookAt(*self.CamereMove())
-
-        for count, machine in enumerate(self.Data["machines"].keys()):
-            if self.Data["machines"][machine]["Visible"] == "False":
-                continue
-
-            Xmin = self.Data["machines"][machine]["data"][0]  # / 1000.0
-            Xmax = self.Data["machines"][machine]["data"][1]  # / 1000.0
-            Ymin = self.Data["machines"][machine]["data"][2]  # / 1000.0
-            Ymax = self.Data["machines"][machine]["data"][3]  # / 1000.0
-            Zmin = self.Data["machines"][machine]["data"][4]  # / 1000.0
-            Zmax = self.Data["machines"][machine]["data"][5]  # / 1000.0
-            color = self.hex_to_rgb(self.Data["machines"][machine]["color"])
-
-            if self.Data["origo"][0][1]:
-                Xmin = int(self.Data["origo"][0][0]) - Xmin
-                Xmax = int(self.Data["origo"][0][0]) - Xmax
-            if self.Data["origo"][1][1]:
-                Ymin = int(self.Data["origo"][1][0]) - Ymin
-                Ymax = int(self.Data["origo"][1][0]) - Ymax
-
-            self.draw_machine(Xmin, Xmax, Ymin, Ymax, Zmin, Zmax,
-                              color)
-
-        xmin = int(self.Data["origo"][0][0]) + (int(self.Data["origo"][0][0]) * -0.001)
-        xmax = int(self.Data["origo"][0][0]) + (int(self.Data["origo"][0][0]) * 0.001)
-        ymin = int(self.Data["origo"][1][0]) + (int(self.Data["origo"][1][0]) * -0.001)
-        ymax = int(self.Data["origo"][1][0]) + (int(self.Data["origo"][1][0]) * 0.001)
-
-        if self.Data["origo"][0][1]:
-            xmin = int(self.Data["origo"][0][0]) - xmin
-            xmax = int(self.Data["origo"][0][0]) - xmax
-        if self.Data["origo"][1][1]:
-            ymin = int(self.Data["origo"][1][0]) - ymin
-            ymax = int(self.Data["origo"][1][0]) - ymax
-
-        zmin = 0
-        zmax = 100
-
-        self.draw_machine(xmin, xmax, ymin,
-                          ymax, zmin, zmax,
-                          (1, 0, 0))
-
-    def hex_to_rgb(self, value):
-        value = value.lstrip('#')
-        lv = len(value)
-        return tuple(int(value[i:i + lv // 3], 16) / 255 for i in range(0, lv, lv // 3))
-
-    def Lines(self, a, b, c, d, vertices):
-        # draw a quad
-        glBegin(GL_QUADS)
-        # glNormal3fv(self.normals[n])
-        glVertex3fv(vertices[a])
-        glVertex3fv(vertices[b])
-        glVertex3fv(vertices[c])
-        glVertex3fv(vertices[d])
-        glEnd()
-
-    # def keyPressEvent(self, event):
-
-    def keyReleaseEvent(self, event):
-        if event.key() == QtCore.Qt.Key_W:
-            self.fwd = False
-        if event.key() == QtCore.Qt.Key_Up:
-            self.up = False
-        if event.key() == QtCore.Qt.Key_S:
-            self.bwd = False
-        if event.key() == QtCore.Qt.Key_Down:
-            self.down = False
-        if event.key() == QtCore.Qt.Key_A:
-            self.A = False
-        if event.key() == QtCore.Qt.Key_Left:
-            self.left = False
-        if event.key() == QtCore.Qt.Key_D:
-            self.D = False
-        if event.key() == QtCore.Qt.Key_Right:
-            self.right = False
-
-    def keyPressEvent(self, event):
-        if event.key() == QtCore.Qt.Key_W:
-            self.fwd = True
-        if event.key() == QtCore.Qt.Key_Up:
-            self.up = True
-        if event.key() == QtCore.Qt.Key_S:
-            self.bwd = True
-        if event.key() == QtCore.Qt.Key_Down:
-            self.down = True
-        if event.key() == QtCore.Qt.Key_A:
-            self.A = True
-        if event.key() == QtCore.Qt.Key_Left:
-            self.left = True
-
-        if event.key() == QtCore.Qt.Key_D:
-            self.D = True
-
-        if event.key() == QtCore.Qt.Key_Right:
-            self.right = True
-
-    def mousePressEvent(self, event: QMouseEvent) -> None:
-        if event.buttons() == QtCore.Qt.LeftButton:
-            self.mpos = event.pos()
-
-            pos = event.pos()
-            x = pos.x()
-            y = pos.y()
-            a = (GLuint * 1)(0)
-            glReadPixels(x, y, 1, 1, GL_RGB, GL_UNSIGNED_BYTE, a)
-            print(f"{a[0]:2x}")
-
-    def mouseMoveEvent(self, event: QMouseEvent) -> None:
-        if event.buttons() == QtCore.Qt.LeftButton:
-            self.diff = event.pos() - self.mpos
-
-    def wheelEvent(self, event: QWheelEvent) -> None:
-        if event.angleDelta().y() == 120.0:
-            self.cameraPos[0] -= (self.cameraFront[0] * 0.1)
-            self.cameraPos[1] -= (self.cameraFront[1] * 0.1)
-            self.cameraPos[2] -= (self.cameraFront[2] * 0.1)
-        elif event.angleDelta().y() == -120.0:
-            self.cameraPos[0] += (self.cameraFront[0] * 0.1)
-            self.cameraPos[1] += (self.cameraFront[1] * 0.1)
-            self.cameraPos[2] += (self.cameraFront[2] * 0.1)
-
-    def draw_machine(self, Xmin, Xmax, Ymin, Ymax, Zmin, Zmax, color):
-        glPushMatrix()
-        glColor4f(*color, 0.7)
-        glTranslatef(0, 0, 0)
-
-        vertices = [
-            (Xmin, Zmin, Ymax),
-            (Xmax, Zmin, Ymax),
-            (Xmax, Zmax, Ymax),
-            (Xmin, Zmax, Ymax),
-            (Xmin, Zmin, Ymin),
-            (Xmax, Zmin, Ymin),
-            (Xmax, Zmax, Ymin),
-            (Xmin, Zmax, Ymin),
-        ]
-
-        self.Lines(0, 1, 2, 3, vertices)
-        self.Lines(3, 7, 4, 0, vertices)
-        self.Lines(0, 1, 5, 4, vertices)
-        self.Lines(4, 7, 6, 5, vertices)
-        self.Lines(5, 1, 2, 6, vertices)
-        self.Lines(6, 7, 3, 2, vertices)
-
-        glPopMatrix()
+    def setDetailedText(self, text):
+        self.textBrowser.append(text)
 
 
 def main():
---
+++ myopengl.py
@@ -0,0 +1,250 @@
+
+from math import cos, sin, radians
+import numpy
+
+from OpenGL.GL import *
+from OpenGL.GLU import gluPerspective, gluLookAt
+from PyQt5 import QtWidgets, QtCore
+
+from PyQt5.QtGui import QMouseEvent, QWheelEvent
+
+
+class MyOPENGL(QtWidgets.QOpenGLWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.up_down_angle = 0.0
+        self.diff = QtCore.QPoint(0, 0)
+        self.left_right_angle = 0.0
+
+        timer = QtCore.QTimer(self)
+        timer.setInterval(20)  # period, in milliseconds
+        timer.timeout.connect(self.update)
+        timer.start()
+
+        self.cameraPos = [0.0, 0.0, -10]
+        self.cameraFront = [0.0, 0.0, -9]
+        self.cameraUp = [0.0, 1.0, 0.0]
+        self.left_right_angle = -90.0
+
+    def initializeGL(self):
+        glClearColor(0.85, 0.85, 0.85, 1.0)
+        glEnable(GL_DEPTH_TEST)
+
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glEnable(GL_BLEND)
+        self.Data = {}
+        self.Data["machines"] = {}
+        self.Data["origo"] = [[10, False], [10, False], [0, False]]
+        self.fwd = False
+        self.up = False
+        self.bwd = False
+        self.down = False
+        self.A = False
+        self.left = False
+        self.D = False
+        self.right = False
+
+    def resizeGL(self, width, height):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glViewport(1, 1, width, height)
+        gluPerspective(45.0, width / height, 0.1, 200.0)
+
+    def CamereMove(self):
+
+        if self.fwd:
+            self.cameraPos[0] -= (self.cameraFront[0] * 0.5)
+            self.cameraPos[1] -= (self.cameraFront[1] * 0.5)
+            self.cameraPos[2] -= (self.cameraFront[2] * 0.5)
+        if self.bwd:
+            self.cameraPos[0] += (self.cameraFront[0] * 0.5)
+            self.cameraPos[1] += (self.cameraFront[1] * 0.5)
+            self.cameraPos[2] += (self.cameraFront[2] * 0.5)
+        if self.up: self.diff = QtCore.QPoint(0, -100)
+        if self.down: self.diff = QtCore.QPoint(0, +100)
+        if self.left: self.diff = QtCore.QPoint(-100, 0)
+        if self.A:
+            a = numpy.cross(self.cameraFront, self.cameraUp)
+            self.cameraPos[0] += (a[0] * 0.5)
+            self.cameraPos[1] += (a[1] * 0.5)
+            self.cameraPos[2] += (a[2] * 0.5)
+        if self.D:
+            a = numpy.cross(self.cameraFront, self.cameraUp)
+            self.cameraPos[0] -= (a[0] * 0.5)
+            self.cameraPos[1] -= (a[1] * 0.5)
+            self.cameraPos[2] -= (a[2] * 0.5)
+        if self.right:   self.diff = QtCore.QPoint(100, 0)
+
+        self.up_down_angle += self.diff.y() * 0.01
+        self.left_right_angle += self.diff.x() * 0.01
+
+        direction = [0, 0, 0]
+        direction[0] = cos(radians(self.left_right_angle)) * cos(radians(self.up_down_angle))
+        direction[1] = sin(radians(self.up_down_angle))
+        direction[2] = sin(radians(self.left_right_angle)) * cos(radians(self.up_down_angle))
+
+        self.cameraFront = (direction[0], direction[1], direction[2])
+        self.diff = QtCore.QPoint(0, 0)
+
+        return self.cameraPos + (list(map(lambda i, j: i - j, self.cameraPos, self.cameraFront)) + self.cameraUp)
+
+    def paintGL(self):
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glMatrixMode(GL_MODELVIEW)
+
+        glLoadIdentity()
+        gluLookAt(*self.CamereMove())
+
+        for count, machine in enumerate(self.Data["machines"].keys()):
+            if self.Data["machines"][machine]["Visible"] == "False":
+                continue
+
+            Xmin = self.Data["machines"][machine]["data"][0]  # / 1000.0
+            Xmax = self.Data["machines"][machine]["data"][1]  # / 1000.0
+            Ymin = self.Data["machines"][machine]["data"][2]  # / 1000.0
+            Ymax = self.Data["machines"][machine]["data"][3]  # / 1000.0
+            Zmin = self.Data["machines"][machine]["data"][4]  # / 1000.0
+            Zmax = self.Data["machines"][machine]["data"][5]  # / 1000.0
+            color = self.hex_to_rgb(self.Data["machines"][machine]["color"])
+
+            if self.Data["origo"][0][1]:
+                Xmin = int(self.Data["origo"][0][0]) - Xmin
+                Xmax = int(self.Data["origo"][0][0]) - Xmax
+            if self.Data["origo"][1][1]:
+                Ymin = int(self.Data["origo"][1][0]) - Ymin
+                Ymax = int(self.Data["origo"][1][0]) - Ymax
+
+            self.draw_machine(Xmin, Xmax, Ymin, Ymax, Zmin, Zmax,
+                              color)
+
+        xmin = int(self.Data["origo"][0][0]) + (int(self.Data["origo"][0][0]) * -0.001)
+        xmax = int(self.Data["origo"][0][0]) + (int(self.Data["origo"][0][0]) * 0.001)
+        ymin = int(self.Data["origo"][1][0]) + (int(self.Data["origo"][1][0]) * -0.001)
+        ymax = int(self.Data["origo"][1][0]) + (int(self.Data["origo"][1][0]) * 0.001)
+
+        if self.Data["origo"][0][1]:
+            xmin = int(self.Data["origo"][0][0]) - xmin
+            xmax = int(self.Data["origo"][0][0]) - xmax
+        if self.Data["origo"][1][1]:
+            ymin = int(self.Data["origo"][1][0]) - ymin
+            ymax = int(self.Data["origo"][1][0]) - ymax
+
+        zmin = 0
+        zmax = 100
+
+        self.draw_machine(xmin, xmax, ymin,
+                          ymax, zmin, zmax,
+                          (1, 0, 0))
+
+    def hex_to_rgb(self, value):
+        value = value.lstrip('#')
+        lv = len(value)
+        return tuple(int(value[i:i + lv // 3], 16) / 255 for i in range(0, lv, lv // 3))
+
+    def Lines(self, a, b, c, d, vertices):
+        # draw a quad
+        glBegin(GL_QUADS)
+        # glNormal3fv(self.normals[n])
+        glVertex3fv(vertices[a])
+        glVertex3fv(vertices[b])
+        glVertex3fv(vertices[c])
+        glVertex3fv(vertices[d])
+        glEnd()
+
+    # def keyPressEvent(self, event):
+
+    def keyReleaseEvent(self, event):
+        if event.key() == QtCore.Qt.Key_W:
+            self.fwd = False
+        if event.key() == QtCore.Qt.Key_Up:
+            self.up = False
+        if event.key() == QtCore.Qt.Key_S:
+            self.bwd = False
+        if event.key() == QtCore.Qt.Key_Down:
+            self.down = False
+        if event.key() == QtCore.Qt.Key_A:
+            self.A = False
+        if event.key() == QtCore.Qt.Key_Left:
+            self.left = False
+        if event.key() == QtCore.Qt.Key_D:
+            self.D = False
+        if event.key() == QtCore.Qt.Key_Right:
+            self.right = False
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_W:
+            self.fwd = True
+        if event.key() == QtCore.Qt.Key_Up:
+            self.up = True
+        if event.key() == QtCore.Qt.Key_S:
+            self.bwd = True
+        if event.key() == QtCore.Qt.Key_Down:
+            self.down = True
+        if event.key() == QtCore.Qt.Key_A:
+            self.A = True
+        if event.key() == QtCore.Qt.Key_Left:
+            self.left = True
+
+        if event.key() == QtCore.Qt.Key_D:
+            self.D = True
+
+        if event.key() == QtCore.Qt.Key_Right:
+            self.right = True
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        if event.buttons() == QtCore.Qt.LeftButton:
+            self.mpos = event.pos()
+
+            pos = event.pos()
+            x = pos.x()
+            y = pos.y()
+            a = (GLuint * 1)(0)
+            glReadPixels(x, y, 1, 1, GL_RGB, GL_UNSIGNED_BYTE, a)
+            print(f"{a[0]:2x}")
+
+    def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        if event.buttons() == QtCore.Qt.LeftButton:
+            self.diff = event.pos() - self.mpos
+
+    def wheelEvent(self, event: QWheelEvent) -> None:
+        if event.angleDelta().y() == 120.0:
+            self.cameraPos[0] -= (self.cameraFront[0] * 0.1)
+            self.cameraPos[1] -= (self.cameraFront[1] * 0.1)
+            self.cameraPos[2] -= (self.cameraFront[2] * 0.1)
+        elif event.angleDelta().y() == -120.0:
+            self.cameraPos[0] += (self.cameraFront[0] * 0.1)
+            self.cameraPos[1] += (self.cameraFront[1] * 0.1)
+            self.cameraPos[2] += (self.cameraFront[2] * 0.1)
+
+    def draw_machine(self, Xmin, Xmax, Ymin, Ymax, Zmin, Zmax, color):
+        glPushMatrix()
+        glColor4f(*color, 0.7)
+        glTranslatef(0, 0, 0)
+
+        vertices = [
+            (Xmin, Zmin, Ymax),
+            (Xmax, Zmin, Ymax),
+            (Xmax, Zmax, Ymax),
+            (Xmin, Zmax, Ymax),
+            (Xmin, Zmin, Ymin),
+            (Xmax, Zmin, Ymin),
+            (Xmax, Zmax, Ymin),
+            (Xmin, Zmax, Ymin),
+        ]
+
+        self.Lines(0, 1, 2, 3, vertices)
+        self.Lines(3, 7, 4, 0, vertices)
+        self.Lines(0, 1, 5, 4, vertices)
+        self.Lines(4, 7, 6, 5, vertices)
+        self.Lines(5, 1, 2, 6, vertices)
+        self.Lines(6, 7, 3, 2, vertices)
+
+        glPopMatrix()
+
